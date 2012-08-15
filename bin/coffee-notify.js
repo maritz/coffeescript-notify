5ee18f088a8428650847f23294f1e108d2984a86
Moritz Peters <maritz.peters@gmail.com>
2012-08-15 16:59:54 +0200

    Implement the bin wrapper (with gntp support)

--- bin/coffee-notify.js
+++ bin/coffee-notify.js
@@ -2,19 +2,24 @@
 
 var optimist = require('optimist')
   .usage('Usage: coffee-notify [options] path/to/script.coffee\n'+
-    'Wrapper around coffee to send notifications on compile success/error.\n'+
+    'Wrapper around coffee to use coffeescript-notify or gntp.\n'+
     'Note: This automatically adds the -c coffee option.\n'+
-    'In addition to the below options you can also pass some coffee options: -b, --bare, -w, --watch, -l, --lint') // TODO: complete this
+    'In addition to the below options you can also pass some coffee options: -w, --watch, -b, --bare, -l, --lint, -o, --output') // TODO: complete this?
+  .describe('gntp', 'Path to the gntp-send bin. Needs the server argument as well.')
+  ["default"]('gntp', '/usr/bin/gntp-send')
   .describe('server', 'Set the host:port of a gntp server to send the notification to. Hint: common default port is 23053')
+  .describe('password', 'Specify a password for the gntp server.')
+  ["default"]('password', '')
   .describe('appname', 'When using gntp you can set the application name used to register on the gntp host. Default: "coffee"')
-  .describe('timeout', 'Amount in ms to show the notification. Default: 2000')
-  .describe('error-multi', 'Multiplier for error message display time. Default: 3 (resulting in 6000ms for the default timeout)')
+  ["default"]('appname', 'coffee')
+  .describe('ignore-success', "Don't send success notifications.")
   .describe('help', 'Show this short help')
-  .boolean('b', 'w', 'l')
+  .boolean('b', 'w', 'l', 'ignore-success')
   .alias({
     b: 'bare',
     w: 'watch',
-    l: 'lint'
+    l: 'lint',
+    o: 'output'
   })
   .demand(1)
   .check(function(args) {
@@ -23,19 +28,147 @@ var optimist = require('optimist')
     }
     return true;
   })
-  .alias('h', 'help')
+  .alias('h', 'help');
   
 if (optimist.argv.help === true) {
   optimist.showHelp();
   process.exit();
 }
 
-var gntp = false;
-
-var gntp_args = [];
+var cp = require('child_process');
+var fs = require('fs');
 
 var argv = optimist.argv;
 
+var createCoffeeProcess = function (handlers) {
+  var args;
+  args = [];
+  
+  if ( ! argv.server) {
+    args.push('-r', 'coffeescript-notify');
+  }
+  args.push('-c');
+  if (argv.lint) {
+    args.push('-l');
+  }
+  if (argv.watch) {
+    args.push('-w');
+  }
+  if (argv.bare) {
+    args.push('-b');
+  }
+  if (argv.output) {
+    args.push('-o', argv.output);
+  }
+  args.push(argv._.join(' '));
+  
+  //console.log('spawning coffee:', args.join(' '));
+  var coffee = cp.spawn('coffee', args);
+  if (handlers) {
+    if (handlers.data) {
+      coffee.stdout.on('data', handlers.data);
+    }
+    if (handlers.err) {
+      coffee.stderr.on('data', handlers.err);
+    }
+    if (handlers.exit) {
+      coffee.on('exit', handlers.exit);
+    }
+  }
+};
 
-
-console.log('test', argv);
+if (argv.server && argv.gntp) {
+  
+  var gntp_exists = fs.existsSync(argv.gntp);
+  
+  if ( ! gntp_exists) {
+    console.error('The supplied gntp path does not exist:', argv.gntp);
+    process.exit(1);
+  }
+  
+  var icons = {
+    success: __dirname+'/../i/coffee-success.png',
+    failure: __dirname+'/../i/coffee-error.png'
+  };
+  
+  var gntpSend = function (msg, icon) {
+    var args = [];
+    args.push(argv.gntp);
+    args.push('-a', '"'+argv.appname+'"');
+    args.push('-s', argv.server);
+    args.push('-p', '"'+argv.password+'"');
+    args.push('"Coffeescript Notify"'); // title
+    args.push('"'+msg+'"');
+    args.push(icon);
+    //console.log('spawning', argv.gntp, args.join(' '));
+    var gntp = cp.exec(args.join(' '));
+    
+    gntp.stdout.on('data', function (err) {
+      console.log('GNTP Out:', err.toString());
+    });
+    gntp.stderr.on('data', function (err) {
+      console.error('GNTP Error:', err.toString());
+      process.exit(1);
+    });
+    gntp.on('exit', function (code) {
+      if (code !== 0) {
+        console.log('GNTP exited with: ', code);
+      }
+    });
+  };
+  
+  var sendSuccess = function (msg) {
+    if ( ! argv["ignore-success"] ) {
+      gntpSend(msg, icons.success);
+    }
+  };
+  
+  var sendError = function (err) {
+    gntpSend(err, icons.failure);
+  };
+  
+  createCoffeeProcess({
+    data: function (data) {
+      data = data.toString();
+      if (data.indexOf(' - compiled ') !== -1) {
+        sendSuccess(data.substr(data.indexOf(' - ')+3));
+      } else {
+        sendError(data);
+      }
+    },
+    err: function (err) {
+      err = err.toString();
+      if (err === 'path.exists is now called `fs.exists`.\n') {
+        // coffee 1.3 still uses fs.exists, not our problem here
+        return null;
+      }
+      
+      if ( ! argv.watch) {
+        sendError(err);
+      } else {
+        console.error('Error from coffee:', err);
+      }
+    },
+    exit: function (code) {
+      if ( ! argv.watch && code === 0) {
+        sendSuccess('compiled '+argv._);
+      } else {
+        console.log('exiting', code);
+      }
+    }
+  });
+  
+} else {
+  createCoffeeProcess({
+    data: function (data) {
+      console.log(data.toString());
+    },
+    err: function (err) {
+      err = err.toString();
+      if (err !== 'path.exists is now called `fs.exists`.\n') {
+        // coffee 1.3 still uses fs.exists, not our problem here
+        console.log('Error:', err);
+      }
+    }
+  });
+}
--- coffeescript-notify.coffee
+++ coffeescript-notify.coffee
@@ -4,13 +4,12 @@ CoffeeScript = require "coffee-script"
 App = {}
 
 App.notify = (title, message, type) ->
-	args = []
-	timeout = if type == "success" then 1000 else 4000
-	args.push '-t', timeout
-	args.push '-i', "#{__dirname}/i/coffee-#{type}.png"
-	args.push "#{title}", "#{message}"
-	console.log 'notify-send', args.join(' ')
-	cp.spawn 'notify-send', args
+  args = []
+  timeout = if type == "success" then 1000 else 4000
+  args.push '-t', timeout
+  args.push '-i', "#{__dirname}/i/coffee-#{type}.png"
+  args.push "#{title}", "#{message}"
+  cp.spawn 'notify-send', args
 
 CoffeeScript.on 'failure', (error, task) ->
   App.notify 'Coffee-Script faild:',"#{error}","error" 
